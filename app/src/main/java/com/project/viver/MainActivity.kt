cf2536ba493f518b191ccae696e2eecf1f800391
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2025-01-04 09:38:13 -0300

    Configuração da segunda tela padrão concluída

--- app/src/main/java/com/project/viver/MainActivity.kt
+++ app/src/main/java/com/project/viver/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.compose.ViverTheme
+import com.project.viver.ui.theme.ViverTheme
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 
--- app/src/main/java/com/project/viver/ViverScreen.kt
+++ app/src/main/java/com/project/viver/ViverScreen.kt
@@ -2,15 +2,24 @@ package com.project.viver
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -18,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -53,7 +63,7 @@ fun ViverAppTopBar1(
     modifier: Modifier = Modifier
 ) {
     // Verifica se a tela atual é uma das que precisam do topo personalizado
-    val shouldShowCustomTopBar = currentScreen in listOf(
+    val shouldShowTopBar = currentScreen in listOf(
         ViverScreen.Login,
         ViverScreen.ForgotPassword,
         ViverScreen.EmailSent,
@@ -63,7 +73,7 @@ fun ViverAppTopBar1(
         ViverScreen.ConfirmPassword
     )
 
-    if (shouldShowCustomTopBar) {
+    if (shouldShowTopBar) {
         Box(
             modifier = modifier
                 .fillMaxWidth()
@@ -110,7 +120,7 @@ fun ViverAppTopBar1(
             // Adiciona a estela (star) acima do ícone e um pouco à direita
             Icon(
                 painter = painterResource(id = R.drawable.star_trajectory),
-                contentDescription = "Star Trajectory",
+                contentDescription = stringResource(R.string.star_trajectory),
                 modifier = Modifier
                     .offset(x = 130.dp, y = 40.dp) // Desloca a estrela para a direita e para cima
                     .size(width = 174.dp, height = 60.dp),
@@ -120,7 +130,7 @@ fun ViverAppTopBar1(
             // Adiciona o ícone ou título no centro do Box
             Icon(
                 painter = painterResource(id = R.drawable.colorful_logo), // Substitua pelo ícone do seu app
-                contentDescription = "Logo",
+                contentDescription = stringResource(R.string.logo),
                 modifier = Modifier
                     .align(Alignment.Center)
                     .size(width = 249.dp, height = 248.dp),
@@ -130,8 +140,87 @@ fun ViverAppTopBar1(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ViverAppTopBar2(){}
+fun ViverAppTopBar2(
+    currentScreen: ViverScreen,
+    modifier: Modifier = Modifier
+) {
+    val shouldShowTopBar = currentScreen == ViverScreen.Home
+
+    val shouldShowTopBarWithoutProfileIcon = currentScreen in listOf(
+        ViverScreen.Home,
+        ViverScreen.Profile,
+        ViverScreen.Restrictions,
+        ViverScreen.NewList,
+        ViverScreen.Lists,
+        ViverScreen.EspecificList,
+    )
+    if (shouldShowTopBarWithoutProfileIcon) {
+        TopAppBar(
+            colors = TopAppBarDefaults.mediumTopAppBarColors(
+                containerColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .background(
+                    brush = Brush.verticalGradient(
+                        0f to Color(0xFFA8D5BA),
+                        1f to Color(0xFF68B684),
+                    )
+                ),
+            title = {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween, // Isso coloca os botões nas extremidades
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Botão da esquerda
+                    Button(
+                        onClick = { /* TODO: Ação ao clicar */ },
+                        modifier = Modifier
+                            .size(width = 50.dp, height = 50.dp),
+                        shape = CircleShape,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0x40111111),
+                            contentColor = Color.White
+                        ),
+                        contentPadding = PaddingValues(0.dp), // Remove padding do botão
+                        elevation = ButtonDefaults.buttonElevation(0.dp),
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.back),
+                            contentDescription = stringResource(R.string.voltar_para_tela_anterior),
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+
+                    // Botão da direita
+                    if (shouldShowTopBar) {
+                        Button(
+                            onClick = { /* TODO: Ação ao clicar */ },
+                            modifier = Modifier
+                                .size(width = 66.dp, height = 51.dp)
+                                .padding(end = 16.dp), // Ajuste a distância do lado direito
+                            shape = CircleShape,
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0x40111111),
+                                contentColor = Color.White
+                            ),
+                            contentPadding = PaddingValues(0.dp), // Remove padding do botão
+                            elevation = ButtonDefaults.buttonElevation(0.dp),
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.person_icon),
+                                contentDescription = stringResource(R.string.icone_para_ir_para_perfil),
+                                modifier = Modifier.size(30.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
 
 
 
@@ -149,6 +238,7 @@ fun ViverApp(
             Column(modifier = Modifier.padding(innerPadding)) {
                 // Topo customizado
                 ViverAppTopBar1(currentScreen = currentScreen)
+                ViverAppTopBar2(currentScreen = currentScreen)
 
                 // Conteúdo principal
                 NavHost(
@@ -164,45 +254,45 @@ fun ViverApp(
                     composable(route = ViverScreen.Login.name) {
                         LoginScreen()
                     }
-//            composable(route = ViverScreen.SignUp.name) {
-//                SignUpScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.Home.name) {
-//                HomeScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.Profile.name) {
-//                ProfileScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.ForgotPassword.name) {
-//                ForgotPasswordScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.ValidateEmail.name) {
-//                ValidateEmailScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.Restrictions.name) {
-//                RestrictionsScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.NewPassword.name) {
-//                NewPasswordScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.NewList.name) {
-//                NewListScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.EmailSent.name) {
-//                EmailSentScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.EditedPasswordSuccessfully.name) {
-//                EditedPasswordSuccessfullyScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.ConfirmPassword.name) {
-//                ConfirmPasswordScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.EspecificList.name) {
-//                EspecificListScreen(navController = navController)
-//            }
-//            composable(route = ViverScreen.Lists.name) {
-//                ListsScreen(navController = navController)
-//            }
+//                    composable(route = ViverScreen.SignUp.name) {
+//                        SignUpScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.Home.name) {
+//                        HomeScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.Profile.name) {
+//                        ProfileScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.ForgotPassword.name) {
+//                        ForgotPasswordScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.ValidateEmail.name) {
+//                        ValidateEmailScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.Restrictions.name) {
+//                        RestrictionsScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.NewPassword.name) {
+//                        NewPasswordScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.NewList.name) {
+//                        NewListScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.EmailSent.name) {
+//                        EmailSentScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.EditedPasswordSuccessfully.name) {
+//                        EditedPasswordSuccessfullyScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.ConfirmPassword.name) {
+//                        ConfirmPasswordScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.EspecificList.name) {
+//                        EspecificListScreen(navController = navController)
+//                    }
+//                    composable(route = ViverScreen.Lists.name) {
+//                        ListsScreen(navController = navController)
+//                    }
                 }
             }
         }
